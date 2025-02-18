041ac3078c4169c0f31439a7d55ec3efbc7324e9
dyomed93 <marcodiomedi19@gmail.com>
2025-02-18 21:44:31 +0100

    ADD: Button for filter reset

--- app.py
+++ app.py
@@ -52,7 +52,8 @@ if __name__ == "__main__":
     page_module = importlib.import_module(pages[selection])
 
     dynamic_filters = DynamicFilters(global_df, filters=['Movie or Serie', 'Country', 'Director', 'Release Year'])
-    st.button("Reset Filters", on_click=dynamic_filters.reset_filters)
+    with st.sidebar:
+        st.button("Reset Filters", on_click=dynamic_filters.reset_filters)
     dynamic_filters.display_filters(location="sidebar")
     global_df = dynamic_filters.filter_df()
     
